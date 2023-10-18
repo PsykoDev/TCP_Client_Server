975d6d5f05a56d6abc65b4bd7ba107df69e0f249
PsykoDev <45910905+PsykoDev@users.noreply.github.com>
2023-10-18 10:54:45 +0200

    update client c# with async

--- Client.cs
+++ Client.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.IO;
 using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 class Client
 {
-    static void Main()
+    static async Task Main()
     {
         Console.WriteLine("Bienvenue sur le client de chat multicanal!");
         Console.Write("Entrez votre nom d'utilisateur : ");
@@ -13,35 +16,49 @@ class Client
         TcpClient client = new TcpClient("127.0.0.1", 1234);
         NetworkStream stream = client.GetStream();
 
-        // Crée un thread pour lire les messages du serveur
-        var receiveThread = new System.Threading.Thread(() =>
+        byte[] pseudo = Encoding.UTF8.GetBytes(username);
+        await stream.WriteAsync(pseudo, 0, pseudo.Length);
+
+        var cancellationTokenSource = new CancellationTokenSource();
+
+        var receiveThread = new Thread(async () =>
         {
-            StreamReader reader = new StreamReader(stream);
+            var buff = new char[4096];
+            var reader = new StreamReader(stream);
             while (true)
             {
                 try
                 {
-                    string message = reader.ReadLine();
-                    Console.WriteLine(message);
+                    int bytesRead = await reader.ReadAsync(buff, 0, buff.Length);
+                    if (bytesRead == 0)
+                    {
+                        break;
+                    }
+                    Console.WriteLine(new string(buff, 0, bytesRead));
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine(e);
-                    throw;
+                    break;
                 }
-
             }
         });
-        
+
         receiveThread.Start();
 
         while (true)
         {
             string message = Console.ReadLine();
-            byte[] data = System.Text.Encoding.ASCII.GetBytes($"{username}: {message}");
-            stream.Write(data, 0, data.Length);
-        }
-        
+            if (string.IsNullOrEmpty(message))
+            {
+                cancellationTokenSource.Cancel();
+                break;
+            }
 
+            byte[] data = Encoding.UTF8.GetBytes($"{message}\n");
+            await stream.WriteAsync(data, 0, data.Length);
+        }
+
+        client.Close();
     }
 }
